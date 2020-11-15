8fa8dc90f1bb963b92509a7a09b38d48e5333bc7
KainFlorian <flo.kainrathh@gmail.com>
2020-11-15 20:10:45 +0100

    formatting

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -68,7 +68,6 @@ public class Task {
     }
 
     @Override
-
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
