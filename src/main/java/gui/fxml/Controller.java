36a299703d71b7f49de9f6cdf102c864498fed19
KainFlorian <flo.kainrathh@gmail.com>
2020-11-23 19:40:01 +0100

    added fxml

---
+++ src/main/java/gui/fxml/Controller.java
@@ -0,0 +1,28 @@
+package gui.fxml;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class Controller {
+    
+    public TextArea descritptionText;
+    public ListView taskListView;
+    public TextField titelText;
+    public ComboBox subjectComboBox;
+    public Label subjectLabel;
+    public ComboBox abgabeOrtComboBox;
+    public Label abgabeOrtLabel;
+    public Label abgabeDatumLabel;
+    public DatePicker abgabeDatumPicker;
+    public Button addButton;
+    public TextField uhrzeitTextField;
+    public Button cancelButton;
+    public Label descriptionLabel;
+    public Label titelLabel;
+    public Button detailButton;
+    public Button fertigButton;
+
+    public void addTask(ActionEvent actionEvent) {
+
+    }
+}
